bd077f6aafbf4a4c26000ad8d04ccd109f3bcf7c
Diego Fernández Barrera <bigomby@gmail.com>
2017-04-27 14:37:27 +0200

    :green_heart: Add tests

--- src/api/controllers/licenses.ts
+++ src/api/controllers/licenses.ts
@@ -15,30 +15,31 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import * as log4js from "log4js";
+import * as NodeRSA from "node-rsa";
 import * as R from "ramda";
 import * as rambdaFantasy from "ramda-fantasy";
-import * as log4js from "log4js";
-import * as uuid from "uuid/v4";
-import * as NodeRSA from "node-rsa";
 import * as util from "util";
+import * as uuid from "uuid/v4";
 
 import config from "../../config";
 
 const IO = rambdaFantasy.IO;
+const Maybe = rambdaFantasy.Maybe;
 
-const logger = log4js.getLogger('[licenses]');
+const logger = log4js.getLogger("[licenses]");
 
-const key = new NodeRSA(config.key.replace(/\\n/g, "\n"));
+const pem = new NodeRSA(config.key.replace(/\\n/g, "\n"));
 
-interface License {
-  info: object,
-  encoded_info: string,
-  signature: string,
-};
+interface ILicense {
+  info: object;
+  encoded_info: string;
+  signature: string;
+}
 
-interface Key {
+interface IKey {
   sign(data: string): string;
-};
+}
 
 /////////////
 // Helpers //
@@ -53,77 +54,83 @@ const sensorList = {
   227: 100,
   219: 100,
   221: 100,
-  223: 100
-}
+  223: 100,
+};
 
 // safeURLBase64Encode :: string -> string
-const safeURLBase64Encode = (data: string): string =>
+export const safeURLBase64Encode = (data: string): string =>
   new Buffer(data)
-    .toString('base64')
-    .replace(/\//g, '_')
-    .replace(/\+/g, '-');
+    .toString("base64")
+    .replace(/\//g, "_")
+    .replace(/\+/g, "-");
 
 // encodeInfo :: License -> License
-const encodeInfo = (license: License): License =>
+export const encodeInfo = (license: ILicense): ILicense =>
   R.assoc(
     "encoded_info",
     safeURLBase64Encode(JSON.stringify(license.info)),
     license);
 
 // addSignature :: License -> License
-const addSignature = R.curry((key: Key, license: License): License =>
+export const addSignature = R.curry((key: IKey, license: ILicense): ILicense =>
   R.assoc(
     "signature",
     safeURLBase64Encode(key.sign(license.encoded_info)),
     license));
 
-// sendResponse :: Responder -> License -> IO License
-const sendResponse = R.curry((res, license: License) => IO(() => {
+// sendLicense :: Responder -> License -> IO License
+export const sendLicense = R.curry((res, license: ILicense) => IO(() => {
   return res.send(license), license;
 }));
 
-// printLicense :: License -> IO ()
-const printLicense = (license: License) => IO(() => {
-  logger.debug(
-    `Generated new license: \n${util.inspect(license, { colors: true })}`
-  );
-});
+// printLicense :: Logger -> License -> IO ()
+export const printLicense = R.curry((maybeLogger, license: ILicense) => IO(() =>
+  maybeLogger.map((log) =>
+    log.debug(
+      `Generated new license: \n${util.inspect(license, { colors: true })}`,
+    )),
+));
 
 // addDays :: number -> Date -> Date
-const addDays = R.curry((days: number, date: Date): Date =>
-  new Date((date.getTime() / 1000 + 60 * 60 * 24 * days) * 1000)
+export const addDays = R.curry((days: number, date: Date): Date =>
+  new Date((date.getTime() / 1000 + 60 * 60 * 24 * days) * 1000),
 );
 
 // add30Days :: Date -> Date
-const add30Days = addDays(30);
+export const add30Days = addDays(30);
 
 // getUnixEpoch :: Date -> number
-const getUnixEpoch = (date: Date): number => Math.floor(date.getTime() / 1000);
+export const getUnixEpoch = (date: Date): number =>
+  Math.floor(date.getTime() / 1000);
 
 // computeExpireTimestamp :: Date -> number
-const computeExpireTimestamp = R.compose(getUnixEpoch, add30Days);
+export const computeExpireTimestamp = R.compose(getUnixEpoch, add30Days);
 
 //////////////
 // Handlers //
 //////////////
 
-const request = (req, res) => {
+export const requestHandler = R.curry((opts, req, res) => {
   const buildLicense = R.pipe(
     R.assocPath(["info", "uuid"], uuid()),
-    R.assocPath(["info", "cluster_uuid"], req.swagger.params.cluster_info.value.cluster_uuid),
+    R.assocPath(["info", "cluster_uuid"],
+      req.swagger.params.cluster_info.value.cluster_uuid),
     R.assocPath(["info", "expire_at"], computeExpireTimestamp(new Date())),
     R.assocPath(["info", "limit_bytes"], 9223372036854775000),
-    R.assocPath(["info", "sensors"], sensorList),
+    R.assocPath(["info", "sensors"], opts.sensorList),
     R.assocPath(["created_at"], new Date().toISOString()));
 
   const signLicense = R.pipe(
     encodeInfo,
-    addSignature(key),
-    sendResponse(res),
-    R.chain(printLicense),
+    addSignature(opts.pem),
+    sendLicense(res),
+    R.chain(printLicense(opts.logger)),
     IO.runIO.bind(this),
   );
 
   R.pipe(buildLicense, signLicense)({});
-}
-export = { request };
+});
+
+export const request = requestHandler({
+  pem, logger: Maybe.of(logger), sensorList,
+});
